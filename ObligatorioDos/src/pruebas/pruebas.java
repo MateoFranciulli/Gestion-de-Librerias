dd146fa939c09055298d785060f6221e29b45b68
Mateo Franciulli Giraldez <127552276+MateoFranciulli@users.noreply.github.com>
2024-11-07 18:04:10 -0300

    Ventanas aun sin terminar

---
+++ ObligatorioDos/src/pruebas/pruebas.java
@@ -0,0 +1,12 @@
+package pruebas;
+import interfaz.*;
+
+public class pruebas {
+    public static void main(String[] args) {
+        VentanaMenu vent=new VentanaMenu();
+        vent.setVisible(true);
+    
+    
+    }
+ 
+}
